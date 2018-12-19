8d48a5e8cdb5fd8f365e542a058eeb534dec0c17
nickkazan <nicholas.kazan1@gmail.com>
2018-12-19 11:51:18 -0500

    Changed design approach

--- to-do-list/to-do-list/FirstViewController.swift
+++ to-do-list/to-do-list/FirstViewController.swift
@@ -10,22 +10,34 @@ import UIKit
 
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    var list = ["Go to Grocery Store", "Grind for 2 hours", "Workout out"];
-    let alert = UIAlertController(title: "Add an item", message: "", preferredStyle: .alert);
+    struct Items{
+        
+        var title : String!
+        var completed : Bool!
+        
+        init(title: String, completed: Bool) {
+            self.title = title
+            self.completed = completed
+        }
+    }
     
-    
+    let defaults = UserDefaults.standard;
+    var list = [Items]()
     @IBOutlet weak var tableList: UITableView!
-    
+
     @IBAction func addItem(_ sender: Any) {
-        alert.addTextField(configurationHandler: { (textField) -> Void in
-            textField.text = ""
-        });
+        let alert = UIAlertController(title: "Add an item", message: "", preferredStyle: .alert);
+        alert.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "Enter Task"
+        })
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
             let textField = alert?.textFields![0];
             print("Text field: \(textField?.text ?? "")")
             if (textField?.text?.count)! > 0 {
-                self.list.append((textField?.text)!);
+                let item = Items(title: (textField?.text)!, completed: true);
+                self.list.append(item);
                 self.tableList.reloadData();
+                self.defaults.set(self.list, forKey: "list");
             }
         }))
         self.present(alert, animated: true, completion: nil);
@@ -37,7 +49,7 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell");
-        cell.textLabel?.text = list[indexPath.row];
+        cell.textLabel?.text = list[indexPath.row].title;
         cell.backgroundColor = UIColor (red: 0.18, green: 0.71, blue: 1.0, alpha: 1.0);
         return cell;
     }
@@ -51,6 +63,8 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        list = defaults.object(forKey: "list") as? [Items] ?? [Items]();
+
         // Do any additional setup after loading the view, typically from a nib.
         
     }
